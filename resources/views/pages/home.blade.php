dccf2c8ab8593d8b58d034d889a2ef3bc4a43bd1
a-krasilskaya <r02062008@gmail.com>
2026-08-30 00:49:07 +0300

    Responsive container padding, header/footer fixes

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,19 +10,21 @@
     <section class="hero-full">
         <canvas id="networkCanvas" class="hero-full__canvas"></canvas>
         <div class="hero-full__overlay"></div>
-        <div class="hero-full__content">
-            <h1 class="hero-full__title">
-                Проектируем и монтируем СКС, ВОЛС и видеонаблюдение с сервисом 24/7
-            </h1>
-            <p class="hero-full__text">
-                Меняем хаос проводов на сеть, которая просто работает
-            </p>
-            <a href="{{ route('contacts') }}" class="btn btn-primary">Оставить заявку</a>
+        <div class="container hero-full__inner">
+            <div class="hero-full__content">
+                <h1 class="hero-full__title">
+                    Проектируем и монтируем СКС, ВОЛС и видеонаблюдение с сервисом 24/7
+                </h1>
+                <p class="hero-full__text">
+                    Меняем хаос проводов на сеть, которая просто работает
+                </p>
+                <a href="{{ route('contacts') }}" class="btn btn-primary">Оставить заявку</a>
 
-            <div class="hero-full__status">
-                <span><span class="dot"></span>ONLINE 24/7</span>
-                <span>99.98% UPTIME</span>
-                <span>&lt;8MS LATENCY</span>
+                <div class="hero-full__status">
+                    <span><span class="dot"></span>ONLINE 24/7</span>
+                    <span>99.98% UPTIME</span>
+                    <span>&lt;8MS LATENCY</span>
+                </div>
             </div>
         </div>
     </section>
